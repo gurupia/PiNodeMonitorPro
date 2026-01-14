89598b5a8be6747f2db39399ace2c13b57124909
gurupia <gurupia@naver.com>
2026-01-15 01:16:58 +0900

    refactor: Async preflight checks in Program.cs (parallel execution)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PiNodeMonitorWinForm
@@ -14,52 +15,66 @@ namespace PiNodeMonitorWinForm
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // --- Smart Pre-flight Check ---
-            // 0. Check Windows Environment (WSL2, Platform, etc.)
-            bool vmp = NodeUtility.IsWindowsFeatureEnabledAsync("VirtualMachinePlatform").GetAwaiter().GetResult();
-            bool wsl = NodeUtility.IsWindowsFeatureEnabledAsync("Microsoft-Windows-Subsystem-Linux").GetAwaiter().GetResult();
-            bool wslInstalled = NodeUtility.IsWslInstalledAsync().GetAwaiter().GetResult();
-            bool checkEnvironment = vmp && wsl && wslInstalled;
+            // Run pre-flight checks asynchronously to avoid UI thread blocking
+            var preflightResult = Task.Run(async () => await RunPreflightChecksAsync()).GetAwaiter().GetResult();
 
-            // 1. Check Docker
-            bool checkDocker = NodeUtility.IsDockerRunningAsync().GetAwaiter().GetResult();
-            
-            // 2. Check Container (Confirm Node is Set Up)
-            bool checkContainer = false;
-            if (checkDocker)
-            {
-                // We look for 'pi-consensus' OR 'testnet2' container
-                if (NodeUtility.IsContainerExistAsync("pi-consensus").GetAwaiter().GetResult())
-                {
-                    checkContainer = true;
-                    NodeUtility.CurrentContainerName = "pi-consensus";
-                }
-                else if (NodeUtility.IsContainerExistAsync("testnet2").GetAwaiter().GetResult())
-                {
-                    checkContainer = true;
-                    NodeUtility.CurrentContainerName = "testnet2";
-                }
-            }
-
-            // 3. Check Firewall (Simplified)
-            bool checkFirewall = NodeUtility.IsFirewallRulePresentAsync().GetAwaiter().GetResult();
-
-            // Logic: If anything is missing, Show Wizard
-            if (!checkEnvironment || !checkDocker || !checkContainer || !checkFirewall)
+            if (!preflightResult.AllPassed)
             {
                 using (var wizard = new SetupWizardForm())
                 {
                     var result = wizard.ShowDialog();
                     if (result != DialogResult.OK)
                     {
-                        // User cancelled the wizard, exit app
                         return;
                     }
                 }
             }
-            // ------------------------------
 
             Application.Run(new Form1());
+        }
+
+        private static async Task<PreflightResult> RunPreflightChecksAsync()
+        {
+            var result = new PreflightResult();
+
+            // Run checks in parallel for faster startup
+            var vmpTask = NodeUtility.IsWindowsFeatureEnabledAsync("VirtualMachinePlatform");
+            var wslTask = NodeUtility.IsWindowsFeatureEnabledAsync("Microsoft-Windows-Subsystem-Linux");
+            var wslInstalledTask = NodeUtility.IsWslInstalledAsync();
+            var dockerTask = NodeUtility.IsDockerRunningAsync();
+
+            await Task.WhenAll(vmpTask, wslTask, wslInstalledTask, dockerTask);
+
+            result.EnvironmentOk = vmpTask.Result && wslTask.Result && wslInstalledTask.Result;
+            result.DockerOk = dockerTask.Result;
+
+            // Container check (depends on Docker)
+            if (result.DockerOk)
+            {
+                if (await NodeUtility.IsContainerExistAsync("pi-consensus"))
+                {
+                    result.ContainerOk = true;
+                    NodeUtility.CurrentContainerName = "pi-consensus";
+                }
+                else if (await NodeUtility.IsContainerExistAsync("testnet2"))
+                {
+                    result.ContainerOk = true;
+                    NodeUtility.CurrentContainerName = "testnet2";
+                }
+            }
+
+            result.FirewallOk = await NodeUtility.IsFirewallRulePresentAsync();
+
+            return result;
+        }
+
+        private class PreflightResult
+        {
+            public bool EnvironmentOk { get; set; }
+            public bool DockerOk { get; set; }
+            public bool ContainerOk { get; set; }
+            public bool FirewallOk { get; set; }
+            public bool AllPassed => EnvironmentOk && DockerOk && ContainerOk && FirewallOk;
         }
     }
 }
